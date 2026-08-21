d738ae7e17ad647c25ae6a51e31e1c44be08d31a
Vega <vegalin1029@gmail.com>
2026-08-21 18:36:42 +0800

    L9 m15 影片上線

--- src/data/missions.ts
+++ src/data/missions.ts
@@ -12031,6 +12031,7 @@ const L9_M15: Mission = {
     { speaker: 'Coco', line: "Very! And they have already gone home. Brr!", lineZh: "超冷！而且他們已經回家了。好冷！" },
     { speaker: 'Polly', line: "Three doors done! The Time Path is almost open!", lineZh: "三扇門完成！時光道快打開了！" },
   ],
+  videoUrl: 'https://pub-64aaa410cb47427ea27ebe800e54daba.r2.dev/videos/L9/m15.mp4?v=1',
 };
 
 const L9_M16: Mission = {
